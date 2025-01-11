16f3c88675cec102fddb84436d902e54bc6a7e16
Jacob Mazelin <jjmazelin@gmail.com>
2025-01-11 16:17:16 -0500

    Create BlinkLimelight and Center on AprilTag Commands
    Co-Authored-By: frogcodes <147776756+frogcodes@users.noreply.github.com>
    Co-Authored-By: Joey <69287308+Joey0980@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/CenterOnAprilTagCommand.java
@@ -0,0 +1,79 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class CenterOnAprilTagCommand extends Command {
+  private NetworkTable limelightTable = NetworkTableInstance.getDefault().getTable("limelight"); 
+  private NetworkTableEntry limelightX = limelightTable.getEntry("tx");
+  private NetworkTableEntry limelightY = limelightTable.getEntry("ty");
+  private double aprilTagID = limelightTable.getEntry("tid").getDouble(-1);
+  private double kTolerance = 0.5;
+  public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric();
+
+  public CenterOnAprilTagCommand() {
+    
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (this.aprilTagID == -1) {
+      System.out.println("No April Tag Detected.");
+      return;
+    }
+    
+    if (this.limelightX.getDouble(0) > this.kTolerance) {
+      drivetrain.setDefaultCommand(
+            // Drivetrain will execute this command periodically
+            drivetrain.applyRequest(() ->
+                drive.withVelocityX(0) // Drive forward with negative Y (forward)
+                    .withVelocityY(this.limelightX.getDouble(0) * 0.1) // Drive left with negative X (left)
+                    .withRotationalRate(0) // Drive counterclockwise with negative X (left)
+            )
+        );
+    }
+    if (this.limelightX.getDouble(0) < -this.kTolerance) {
+      drivetrain.setDefaultCommand(
+            // Drivetrain will execute this command periodically
+            drivetrain.applyRequest(() ->
+                drive.withVelocityX(0) // Drive forward with negative Y (forward)
+                    .withVelocityY(-this.limelightX.getDouble(0) * 0.1) // Drive left with negative X (left)
+                    .withRotationalRate(0) // Drive counterclockwise with negative X (left)
+            )
+        );
+    }
+    if (Math.abs(this.limelightX.getDouble(0)) <= this.kTolerance) {
+      return;
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
