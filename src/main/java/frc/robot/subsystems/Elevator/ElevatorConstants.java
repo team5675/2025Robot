462046d32bc2af765c0cdc4dfabedf0e46d96a4f
Joey <69287308+Joey0980@users.noreply.github.com>
2025-01-29 20:55:06 -0500

    Create Elevator class

---
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorConstants.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.Elevator;
+
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+
+public final class ElevatorConstants {
+    public static final double motorP = 0.02;
+    public static final double motorI = 0.00;
+    public static final double motorD = 0.00;
+
+    public static final Distance L4_HEIGHT = Meters.of(2.5146);
+	public static final Distance L3_HEIGHT = Meters.of(1.54305);
+	public static final Distance L2_HEIGHT = Meters.of(1.54305);
+	public static final Distance L1_HEIGHT = Meters.of(1);
+	public static final Distance IDLE_HEIGHT = Meters.of(0.1);
+	public static final Distance ALGAE_LOW_HEIGHT = Meters.of(1);
+	public static final Distance ALGAE_HIGH_HEIGHT = Meters.of(1);
+	public static final Distance ALGAE_PROCESSOR_HEIGHT = Meters.of(1);
+
+    public static final Distance METERS_PER_ROTATION = Meters.of(1);
+
+    public static final LinearVelocity ZEROING_VELOCITY = MetersPerSecond.of(0.25);
+}
