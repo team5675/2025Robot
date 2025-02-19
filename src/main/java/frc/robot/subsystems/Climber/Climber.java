36522dd9066679e85c92764e016052e946101c02
PeytonMcMeekan <147776849+PeytonMcMeekan@users.noreply.github.com>
2025-02-19 18:28:57 -0500

    Climb Command and limit switch

--- src/main/java/frc/robot/subsystems/Climber/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber/Climber.java
@@ -11,10 +11,15 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public class Climber extends SubsystemBase {
     private static Climber instance;
+    public static Climber getInstance;
+    public DigitalInput LimitSwitch;
+    public Trigger lsTripped;
     public static Climber getInstance() {
         if (instance == null) {
             instance = new Climber();
@@ -25,15 +30,15 @@ public class Climber extends SubsystemBase {
     SparkMax clawmotor = new SparkMax(32, MotorType.kBrushed);
     SparkMax climberMotor = new SparkMax(22, MotorType.kBrushless);
     public SparkMaxConfig climberMotorConfig;
-    SparkAbsoluteEncoder angleEncoder;
 
     public Climber() {
-        angleEncoder = climberMotor.getAbsoluteEncoder();
         climberMotorConfig = new SparkMaxConfig();
         climberMotorConfig.idleMode(IdleMode.kBrake);
         climberMotorConfig.smartCurrentLimit(15);
         climberMotor.configure(climberMotorConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
         //
+        LimitSwitch = new DigitalInput(2);
+        lsTripped = new Trigger(LimitSwitch::get);
     }
 
     public void runclimber() {
