a0ceecb38f3fe66ce1d763cf86d50cb12819a694
Joey <kindest.yum_7d@icloud.com>
2025-02-18 20:26:39 -0500

    Create LED.java

---
+++ src/main/java/frc/robot/subsystems/LED/LED.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.LED;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LED extends SubsystemBase {
+  private static LED instance;
+
+  public static LED getInstance() {
+    if (instance == null) {
+      instance = new LED();
+    }
+    return instance;
+  }
+
+  public AddressableLED led;
+  public AddressableLEDBuffer ledBuffer;
+
+  public LED() {
+    led = new AddressableLED(0);
+    ledBuffer = new AddressableLEDBuffer(90);
+    led.setLength(ledBuffer.getLength());
+
+    led.setData(ledBuffer);
+    led.start();
+  }
+  
+  @Override
+  public void periodic() {
+
+  }
+}
