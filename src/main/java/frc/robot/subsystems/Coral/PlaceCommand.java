5e4653106d779ebde8b94fdeb129176d958fa612
Joey <kindest.yum_7d@icloud.com>
2025-02-26 18:48:29 -0500

    Move PlaceCommand

--- src/main/java/frc/robot/subsystems/Coral/PlaceCommand.java
+++ src/main/java/frc/robot/subsystems/Coral/PlaceCommand.java
@@ -2,10 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.subsystems.Coral;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Coral.Coral;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class PlaceCommand extends Command {
