897a3817e3e5bee3cc3bd9f6c137c12dbe3e7edd
Jacob Mazelin <jjmazelin@gmail.com>
2025-03-04 18:37:39 -0500

    Added SignalLogger

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.SignalLogger;
+
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -17,6 +19,8 @@ public class Robot extends TimedRobot {
   public Robot() {
     m_robotContainer = new RobotContainer();
     CameraServer.startAutomaticCapture();
+
+    SignalLogger.start();
     
   }
 
