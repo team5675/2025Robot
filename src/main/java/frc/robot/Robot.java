165e9932d3ad647cec4c45081950492d7078bd66
Jacob Mazelin <jjmazelin@gmail.com>
2025-03-08 19:33:40 -0500

    Added Pathplanner Warmup Command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import com.ctre.phoenix6.SignalLogger;
+import com.pathplanner.lib.commands.PathfindingCommand;
 
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.DataLogManager;
@@ -20,6 +21,7 @@ public class Robot extends TimedRobot {
   public Robot() {
     m_robotContainer = new RobotContainer();
     CameraServer.startAutomaticCapture();
+    PathfindingCommand.warmupCommand().schedule();
 
     //SignalLogger.start();
     
