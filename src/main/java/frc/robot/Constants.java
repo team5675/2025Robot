0df289494db61934f2778ee16c0202452c4b55fc
Jacob Mazelin <jjmazelin@gmail.com>
2025-03-19 20:54:12 -0400

    Pre LED Release

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,7 +21,7 @@ public class Constants {
     public class PathplannerConstants {
         // Create the constraints to use while pathfinding
         public static final PathConstraints constraints = new PathConstraints(
-        2, 2.5,
+        2, 2,
             Units.degreesToRadians(360), Units.degreesToRadians(540));
     }
 }
