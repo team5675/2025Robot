64a8089db7de49a49b69477ddc50171536e841f2
Jacob Mazelin <jjmazelin@gmail.com>
2025-03-20 21:27:09 -0400

    Update Positions (GVSU)

--- src/main/java/frc/robot/subsystems/Swerve/AlignmentConstants.java
+++ src/main/java/frc/robot/subsystems/Swerve/AlignmentConstants.java
@@ -17,54 +17,54 @@ public class AlignmentConstants {
     private static final Rotation2d BARGE = new Rotation2d(Math.toRadians(180));
     private static final Rotation2d PROCESSORANGLE = new Rotation2d(Math.toRadians(-90));
 
-    public static final Pose2d REEF_A = new Pose2d(3.213, 4.204, AB);
+    // public static final Pose2d REEF_A = new Pose2d(3.213, 4.204, AB);
    
-    public static final Pose2d REEF_B = new Pose2d(3.245, 3.821, AB);
+    // public static final Pose2d REEF_B = new Pose2d(3.245, 3.821, AB);
    
-    public static final Pose2d REEF_C = new Pose2d(3.695, 3.008, CD);
+    // public static final Pose2d REEF_C = new Pose2d(3.695, 3.008, CD);
    
-    public static final Pose2d REEF_D = new Pose2d(4.005, 2.862, CD);
+    // public static final Pose2d REEF_D = new Pose2d(4.005, 2.862, CD);
    
-    public static final Pose2d REEF_E = new Pose2d(4.970, 2.839, EF);
+    // public static final Pose2d REEF_E = new Pose2d(4.970, 2.839, EF);
     
-    public static final Pose2d REEF_F = new Pose2d(5.265, 3.037, EF);
+    // public static final Pose2d REEF_F = new Pose2d(5.265, 3.037, EF);
     
-    public static final Pose2d REEF_G = new Pose2d(5.762, 3.851, GH);
+    // public static final Pose2d REEF_G = new Pose2d(5.762, 3.851, GH);
     
-    public static final Pose2d REEF_H = new Pose2d(5.73, 4.21, GH);
+    // public static final Pose2d REEF_H = new Pose2d(5.73, 4.21, GH);
     
-    public static final Pose2d REEF_I = new Pose2d(5.274, 5.037, IJ);
+    // public static final Pose2d REEF_I = new Pose2d(5.274, 5.037, IJ);
     
-    public static final Pose2d REEF_J = new Pose2d(4.939, 5.198, IJ);
+    // public static final Pose2d REEF_J = new Pose2d(4.939, 5.198, IJ);
     
-    public static final Pose2d REEF_K = new Pose2d(3.989, 5.201, KL);
+    // public static final Pose2d REEF_K = new Pose2d(3.989, 5.201, KL);
     
-    public static final Pose2d REEF_L = new Pose2d(3.707, 5.010, KL);
+    // public static final Pose2d REEF_L = new Pose2d(3.707, 5.010, KL);
 
     //St Joe Locations
-    // public static final Pose2d REEF_A = new Pose2d(3.236915, 4.197185, AB);
+    public static final Pose2d REEF_A = new Pose2d(3.237, 4.199, AB);
    
-    // public static final Pose2d REEF_B = new Pose2d(3.251745, 3.85543, AB);
+    public static final Pose2d REEF_B = new Pose2d(3.2606, 3.8572, AB);
+    //Changed C
+    public static final Pose2d REEF_C = new Pose2d(3.72599, 3.02979, CD);
+    //D works
+    public static final Pose2d REEF_D = new Pose2d(4.021, 2.8778, CD);
    
-    // public static final Pose2d REEF_C = new Pose2d(3.72599, 3.02979, CD);
-   
-    // public static final Pose2d REEF_D = new Pose2d(4.011477, 2.874221, CD);
-   
-    // public static final Pose2d REEF_E = new Pose2d(4.9749, 2.8531, EF);
+    public static final Pose2d REEF_E = new Pose2d(4.9572, 2.849, EF);
     
-    // public static final Pose2d REEF_F = new Pose2d(5.24874, 3.0349, EF);
+    public static final Pose2d REEF_F = new Pose2d(5.256, 3.0469, EF);
     
-    // public static final Pose2d REEF_G = new Pose2d(5.7392, 3.8541, GH);
+    public static final Pose2d REEF_G = new Pose2d(5.7419, 3.853, GH);
     
-    // public static final Pose2d REEF_H = new Pose2d(5.7202, 4.1968, GH);
+    public static final Pose2d REEF_H = new Pose2d(5.7217, 4.19517, GH);
     
-    // public static final Pose2d REEF_I = new Pose2d(5.2662, 5.0143, IJ);
+    public static final Pose2d REEF_I = new Pose2d(5.2577, 5.0261, IJ);
+    //J works
+    public static final Pose2d REEF_J = new Pose2d(4.9627, 5.1784, IJ);
     
-    // public static final Pose2d REEF_J = new Pose2d(4.9596, 5.17618, IJ);
+    public static final Pose2d REEF_K = new Pose2d(4.00304, 5.1913, KL);
     
-    // public static final Pose2d REEF_K = new Pose2d(4.02227, 5.19255, KL);
-    
-    // public static final Pose2d REEF_L = new Pose2d(3.7347, 5.014789, KL);
+    public static final Pose2d REEF_L = new Pose2d(3.7185, 4.993, KL);
     
     public static final Pose2d CORAL1RIGHT = new Pose2d(1.536, 0.726, RIGHTCORALSTATION);
     public static final Pose2d CORAL1LEFT = new Pose2d(0.800, 1.258, RIGHTCORALSTATION);
@@ -79,29 +79,29 @@ public class AlignmentConstants {
     public static final Pose2d PROCESSOR = new Pose2d(5.979, 0.541, PROCESSORANGLE);
 
     //Home Locations
-    public static final Pose2d ALGAE_AB = new Pose2d(3.1876, 4.026, AB);
+    // public static final Pose2d ALGAE_AB = new Pose2d(3.1876, 4.026, AB);
 
-    public static final Pose2d ALGAE_CD = new Pose2d(3.839, 2.899, CD);
+    // public static final Pose2d ALGAE_CD = new Pose2d(3.839, 2.899, CD);
 
-    public static final Pose2d ALGAE_EF = new Pose2d(5.1396, 2.899, EF);
+    // public static final Pose2d ALGAE_EF = new Pose2d(5.1396, 2.899, EF);
 
-    public static final Pose2d ALGAE_GH = new Pose2d(5.7909, 4.0258, GH);
+    // public static final Pose2d ALGAE_GH = new Pose2d(5.7909, 4.0258, GH);
 
-    public static final Pose2d ALGAE_IJ = new Pose2d(5.1396, 5.15242, IJ);
+    // public static final Pose2d ALGAE_IJ = new Pose2d(5.1396, 5.15242, IJ);
     
-    public static final Pose2d ALGAE_KL = new Pose2d(3.8389, 5.1524, KL);
+    // public static final Pose2d ALGAE_KL = new Pose2d(3.8389, 5.1524, KL);
 
     // Algae center positions St. Joe
-    // public static final Pose2d ALGAE_AB = new Pose2d(3.237, 4.0258, AB);
+    public static final Pose2d ALGAE_AB = new Pose2d(3.2488, 4.0281, AB);
 
-    // public static final Pose2d ALGAE_CD = new Pose2d(3.8679, 2.950, CD);
+    public static final Pose2d ALGAE_CD = new Pose2d(3.8719, 2.9510, CD);
 
-    // public static final Pose2d ALGAE_EF = new Pose2d(5.0976, 2.943, EF);
+    public static final Pose2d ALGAE_EF = new Pose2d(5.1066, 2.9479, EF);
 
-    // public static final Pose2d ALGAE_GH = new Pose2d(5.7309, 4.0248, GH);
+    public static final Pose2d ALGAE_GH = new Pose2d(5.7318, 4.0241, GH);
 
-    // public static final Pose2d ALGAE_IJ = new Pose2d(5.1127, 5.095, IJ);
+    public static final Pose2d ALGAE_IJ = new Pose2d(5.1102, 5.10225, IJ);
     
-    // public static final Pose2d ALGAE_KL = new Pose2d(3.8779, 5.1044, KL);
+    public static final Pose2d ALGAE_KL = new Pose2d(3.86077, 5.109215, KL);
 
 }
